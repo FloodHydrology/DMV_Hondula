abf57e8795e29f2fef9cf0751711f2480171ab41
Nate Jones <cnjones7@ua.edu>
2021-05-24 17:30:05 -0500

    watershed shapes

--- RScripts/watershed_shapes.R
+++ RScripts/watershed_shapes.R
@@ -11,75 +11,89 @@
 #Clear memory
 rm(list=ls(all=TRUE))
 
-#Defin relevant working directories
-data_dir<-"//nfs/palmer-group-data/choptank/Nate/Spatial_Analysis/"
+#Define temp dir
+dir.create(paste0(getwd(), '/data/temp'))
+temp_dir<-paste0(getwd(), '/data/temp/')
 
 #Download packages 
 library(sf)
 library(raster)
+library(stars)
 library(whitebox)
 library(leaflet)
 library(tidyverse)
 
 #Load DEM 
-dem<-raster('data/I_Input/2007_1m_DEM.tif')
-  dem[dem<0]<-NA
+dem<-raster('data/dem.tif')
 
 #Load outlet information
-wells<-st_read('data/I_input/well_location.shp') %>% 
+wells<-st_read('data/well_location.shp') %>% 
   #Filter to points of interest
   filter(
-    Site_ID == 'Jones Road South Catchment Outlet' |
+    Site_ID == 'Jones Road South Catchment Outlet' | 
     Site_ID == 'Mikey Likey Catchment Outlet') 
+
+#Load wetland shape
+wetlands<-st_read('data/wetlands.shp')
   
+
+
 #Plot for funzies
 plot(dem)
+wetlands %>% st_geometry() %>% plot(., add=T, col="dark blue")
 wells %>% st_geometry() %>% plot(., add=T, col="dark red", pch=19, cex=3)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #2.0 Watershed Delineation------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#2.1 Setup WBT workspace ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Define temp dir
-temp_dir<-'C://WorkspaceR//DMV_Hondula//data//II_Scratch//'
-
-#Export DEM and pp to tmep dir
-writeRaster(dem, paste0(temp_dir,"dem.tif"))
-st_write(wells, paste0(temp_dir,"pp.shp"))
-
+#2.1 Burn Wetlands into DEM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Create DEM mask
+mask<-rasterize(wetlands, dem, 1)
+dem_mask<-mask*dem
+  
+#Create minimum raster
+dem_min<-cellStats(dem_mask, base::min, na.rm=T)
+dem_min<-dem_mask*0+dem_min
+dem_min[is.na(dem_min)]<-0
+  
+#Replace masked location with min raster value
+dem_mask<-dem_mask*0
+dem_mask[is.na(dem_mask)]<-1
+dem<-dem*dem_mask+dem_min
+  
 #2.2 Prep DEM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#fill single cell pits
-wbt_fill_single_cell_pits(
-  dem = 'dem.tif', 
-  output = 'dem_filled.tif',
-  wd = temp_dir
-)
+#Export DEM to tmep dir
+writeRaster(dem, paste0(temp_dir,"dem.tif"), overwrite=T)
 
 #Apply gaussian filter
 wbt_gaussian_filter(
-  input = 'dem_filled.tif',
+  input = 'dem.tif',
   output = 'dem_filter.tif',
   wd = temp_dir
 )
 
 #breach depressions
 wbt_breach_depressions(
-  dem = 'dem.tif',
+  dem = 'dem_filter.tif',
   output = 'dem_breached.tif',
+  fill_pits = T,
   wd = temp_dir
 )
 
 #2.3 Watershed Delineation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Export pour points to temp dir
+st_write(wells, paste0(temp_dir,"pp.shp"), delete_dsn =T)
+
 #Flow Direction
-wbt_fd8_pointer(
+wbt_d8_pointer(
   dem='dem_breached.tif',
   output = 'fdr.tif',
   wd = temp_dir
 )
 
 #Flow accumulation
-wbt_fd8_flow_accumulation(
-  dem = 'dem_breached.tif',
+wbt_d8_flow_accumulation(
+  input = 'dem_breached.tif',
   output = 'fac.tif',
   wd = temp_dir
 )
@@ -101,19 +115,23 @@ wbt_jenson_snap_pour_points(
   wd = temp_dir
 )
 
-#Convert pour points to raster
-wbt_vector_points_to_raster(
-  input = 'snap.shp',
-  output = 'snap.tif',
-  base = 'dem_breached.tif',
-  wd = temp_dir
-)
-
 #Watershed Delineation
 wbt_watershed(
-  d8_pntr = 'fdr.tif',
-  pour_pts = 'snap.tif',
-  output = 'watershed.tif',
-  wd = temp_dir,  
-  verbose_mode = T
+  d8_pntr = "fdr.tif",
+  pour_pts = "snap.shp", 
+  output = "sheds.tif" ,
+  wd=temp_dir
 )
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#3.0 Export Shapes -------------------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Create polygon from raster
+sheds<-raster(paste0(temp_dir, 'sheds.tif'))
+sheds<-sheds %>% st_as_stars(.) %>% st_as_sf(., merge=T)
+
+#Export watershed shapes
+st_write(sheds, "docs/sheds.shp")
+
+#Delete temp folder
+unlink("data/temp", recursive = T)
