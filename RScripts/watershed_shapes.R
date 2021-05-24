609129a304ea3ed7b047041f4c367ac7a8be5caa
Nate Jones <cnjones7@ua.edu>
2021-05-24 08:50:33 -0500

    initial watershed delineation

--- RScripts/watershed_shapes.R
+++ RScripts/watershed_shapes.R
@@ -17,6 +17,7 @@ data_dir<-"//nfs/palmer-group-data/choptank/Nate/Spatial_Analysis/"
 #Download packages 
 library(sf)
 library(raster)
+library(whitebox)
 library(leaflet)
 library(tidyverse)
 
@@ -38,5 +39,81 @@ wells %>% st_geometry() %>% plot(., add=T, col="dark red", pch=19, cex=3)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #2.0 Watershed Delineation------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#2.1 Setup WBT workspace ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Define temp dir
 temp_dir<-'C://WorkspaceR//DMV_Hondula//data//II_Scratch//'
+
+#Export DEM and pp to tmep dir
+writeRaster(dem, paste0(temp_dir,"dem.tif"))
+st_write(wells, paste0(temp_dir,"pp.shp"))
+
+#2.2 Prep DEM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#fill single cell pits
+wbt_fill_single_cell_pits(
+  dem = 'dem.tif', 
+  output = 'dem_filled.tif',
+  wd = temp_dir
+)
+
+#Apply gaussian filter
+wbt_gaussian_filter(
+  input = 'dem_filled.tif',
+  output = 'dem_filter.tif',
+  wd = temp_dir
+)
+
+#breach depressions
+wbt_breach_depressions(
+  dem = 'dem.tif',
+  output = 'dem_breached.tif',
+  wd = temp_dir
+)
+
+#2.3 Watershed Delineation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Flow Direction
+wbt_fd8_pointer(
+  dem='dem_breached.tif',
+  output = 'fdr.tif',
+  wd = temp_dir
+)
+
+#Flow accumulation
+wbt_fd8_flow_accumulation(
+  dem = 'dem_breached.tif',
+  output = 'fac.tif',
+  wd = temp_dir
+)
+
+#Create streams raster
+wbt_reclass(
+  input = 'fac.tif',
+  output = 'streams.tif',
+  reclass_vals = '0;1;40000;1;40001;15000000', 
+  wd= temp_dir
+)
+
+#Snap pour point to streams raster
+wbt_jenson_snap_pour_points(
+  pour_pts = 'pp.shp',
+  streams = 'streams.tif',
+  snap_dist = 100,
+  output = 'snap.shp',
+  wd = temp_dir
+)
+
+#Convert pour points to raster
+wbt_vector_points_to_raster(
+  input = 'snap.shp',
+  output = 'snap.tif',
+  base = 'dem_breached.tif',
+  wd = temp_dir
+)
+
+#Watershed Delineation
+wbt_watershed(
+  d8_pntr = 'fdr.tif',
+  pour_pts = 'snap.tif',
+  output = 'watershed.tif',
+  wd = temp_dir,  
+  verbose_mode = T
+)
